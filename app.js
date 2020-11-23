bb5c29f61507874b15bea016bdc28cc2a1074da4
yun <n856839@gmail.com>
2020-11-23 22:08:11 +0800

    feat: add record seeder & handlebars

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 const app = express()
 const PORT = 3000
 
+
+// ------------- mongodb 設定 -------------
+mongoose.connect('mongodb://localhost/expense', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+// ------------- mongodb 設定 -------------
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('hi')
+  res.render('index')
 })
 
 app.listen(PORT, () => {
---
+++ models/seeds/recordSeeder.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+const Record = require('../record')
+
+mongoose.connect('mongodb://localhost/expense', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+  Record.create(
+    {
+      name: '午餐',
+      category: '餐飲食品',
+      date: 2019 / 4 / 23,
+      amount: 60,
+      icon: 'fas fa-utensils'
+    },
+    {
+      name: '晚餐',
+      category: '餐飲食品',
+      date: 2019 / 4 / 23,
+      amount: 60,
+      icon: 'fas fa-utensils'
+    },
+    {
+      name: '捷運',
+      category: '交通出行',
+      date: 2019 / 4 / 23,
+      amount: 120,
+      icon: 'fas fa-shuttle-van'
+    },
+    {
+      name: '電影：驚奇隊長',
+      category: '休閒娛樂',
+      date: 2019 / 4 / 23,
+      amount: 220,
+      icon: 'fas fa-grin-beam'
+    },
+    {
+      name: '租金',
+      category: '家居物業',
+      date: 2019 / 4 / 23,
+      amount: 25000,
+      icon: 'fas fa-home'
+    })
+  console.log('done')
+})

a4784f4644d9d64083135df1e2f9722bdcbf2cf5
yun <n856839@gmail.com>
2021-01-22 16:44:19 +0800

    feat: add user2 to seeder

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -9,30 +9,39 @@ const User = require('../user')
 const db = require('../../config/mongoose')
 const recordList = require('../../record.json').results
 
-const SEED_USER = {
-  name: 'root',
-  email: 'root@example.com',
-  password: '12345678'
-}
+const SEED_USERS = [
+  {
+    email: 'user1@example.com',
+    password: '12345678'
+  },
+  {
+    email: 'user2@example.com',
+    password: '12345678'
+  }
+]
 
 db.once('open', () => {
-  bcrypt
-    .genSalt(10)
-    .then(salt => bcrypt.hash(SEED_USER.password, salt))
-    .then(hash => User.create({
-      email: SEED_USER.email,
-      password: hash
-    })
-      .then(user => {
-        const userId = user._id
-        return Promise.all(Array.from(
-          { length: recordList.length },
-          (_, i) => {
-            return Record.create(Object.assign(recordList[i], { userId }))
-          }))
-      }))
-    .then(() => {
-      console.log('recordSeeder is done!')
-      process.exit()
-    })
+  SEED_USERS.forEach((user, index) => {
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(user.password, salt))
+      .then(hash => User.create({
+        email: user.email,
+        password: hash
+      })
+        .then(user => {
+          const userId = user._id
+          return Promise.all(
+            Array.from(
+              { length: 3 },
+              (_, i) => {
+                return Record.create(Object.assign(recordList[i + index * 3], { userId }))
+              }))
+        }))
+      .then(() => {
+        console.log('recordSeeder is done!')
+        process.exit()
+      })
+  })
+
 })
